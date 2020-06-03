170b50893b27e6e4a966d6c2ae9e9c90824e47c9
DC_LangX <dc_langx@126.com>
2020-06-03 14:52:39 +0800

    引入mockjs

--- vue.config.js
+++ vue.config.js
@@ -45,12 +45,13 @@ module.exports = {
   productionSourceMap: false, // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
   devServer: {
     port: 9020, // 端口
-    open: false, // 启动后打开浏览器
+    open: true, // 启动后打开浏览器
     overlay: {
       //  当出现编译器错误或警告时，在浏览器中显示全屏覆盖层
       warnings: false,
       errors: true
-    }
+    },
+    before: require('./mock/mock-server.js')
     // proxy: {
     //   //配置跨域
     //   '/api': {
@@ -64,7 +65,7 @@ module.exports = {
     // }
   },
   css: {
-    extract: IS_PROD, //是否将组件中的 CSS 提取至一个独立的 CSS 文件中 (而不是动态注入到 JavaScript 中的 inline 代码)。
+    extract: IS_PROD, // 是否将组件中的 CSS 提取至一个独立的 CSS 文件中 (而不是动态注入到 JavaScript 中的 inline 代码)。
     sourceMap: false,
     loaderOptions: {
       scss: {
